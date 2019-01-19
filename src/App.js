f304396f7d29cb693f5f5fffc3076cf8668889dd
azax25547 <momentumangular@gmail.com>
2019-01-20 01:09:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Callback from "./components/Callback";
+import Profile from "./components/Profile";
+import Auth from "./Auth/Auth";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = new Auth(props.history);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Nav />
+        <Route
+          exact={true}
+          render={props => <Home auth={this.state} {...props} />}
+          path="/"
+        />
+        <Route
+          render={props => <Callback auth={this.state} {...props} />}
+          path="/callback"
+        />
+        <Route component={Profile} path="/profile" />
+      </>
     );
   }
 }
